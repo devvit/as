f65d605fb3f1d413332b114e4f672052ff990603
ymca <admin@example.com>
2023-07-22 18:05:27 +0800

    Sat Jul 22 18:05:25 CST 2023  M makeconf.sh

--- makeconf.sh
+++ makeconf.sh
@@ -6,25 +6,25 @@ for distfile in $(ls ./etc/*.dist); do
     f=$(dirname $distfile)/$(basename $distfile .dist)
     cp $distfile $f
     perl -pi -e 's/127.0.0.1;3306;acore;acore;/127.0.0.1;4306;acore;acore;/g' $f
-    perl -pi -e 's/DataDir(\s*)=/DataDir = ".\/data" #/g' $f
-    perl -pi -e 's/SOAP.Enabled(\s*)=/SOAP.Enabled = 1 #/g' $f
+    perl -pi -e 's/^DataDir(\s*)=/DataDir = ".\/data" #/g' $f
+    perl -pi -e 's/^SOAP.Enabled(\s*)=/SOAP.Enabled = 1 #/g' $f
 
-    perl -pi -e "s/Rate.XP.Kill(\s*)=/Rate.XP.Kill = $whiz #/g" $f
-    perl -pi -e "s/Rate.XP.Quest(\s*)=/Rate.XP.Quest = $whiz #/g" $f
-    perl -pi -e "s/Rate.XP.Quest.DF(\s*)=/Rate.XP.Quest.DF = $whiz #/g" $f
-    perl -pi -e "s/Rate.XP.Explore(\s*)=/Rate.XP.Explore = $whiz #/g" $f
-    perl -pi -e "s/Rate.XP.Pet(\s*)=/Rate.XP.Pet = $whiz #/g" $f
+    perl -pi -e "s/^Rate.XP.Kill(\s*)=/Rate.XP.Kill = $whiz #/g" $f
+    perl -pi -e "s/^Rate.XP.Quest(\s*)=/Rate.XP.Quest = $whiz #/g" $f
+    perl -pi -e "s/^Rate.XP.Quest.DF(\s*)=/Rate.XP.Quest.DF = $whiz #/g" $f
+    perl -pi -e "s/^Rate.XP.Explore(\s*)=/Rate.XP.Explore = $whiz #/g" $f
+    perl -pi -e "s/^Rate.XP.Pet(\s*)=/Rate.XP.Pet = $whiz #/g" $f
 
-    perl -pi -e "s/Rate.Honor(\s*)=/Rate.Honor = $whiz #/g" $f
-    perl -pi -e "s/Rate.ArenaPoints(\s*)=/Rate.ArenaPoints = $whiz #/g" $f
+    perl -pi -e "s/^Rate.Honor(\s*)=/Rate.Honor = $whiz #/g" $f
+    perl -pi -e "s/^Rate.ArenaPoints(\s*)=/Rate.ArenaPoints = $whiz #/g" $f
 
-    perl -pi -e "s/Rate.Drop.Money(\s*)=/Rate.Drop.Money = $whiz #/g" $f
+    perl -pi -e "s/^Rate.Drop.Money(\s*)=/Rate.Drop.Money = $whiz #/g" $f
 
-    perl -pi -e 's/NpcBot.MaxBots(\s*)=/NpcBot.MaxBots = 39 #/g' $f
-    perl -pi -e 's/NpcBot.Enable.Raid(\s*)=/NpcBot.Enable.Raid = 1 #/g' $f
-    perl -pi -e 's/NpcBot.Enable.BG(\s*)=/NpcBot.Enable.BG = 1 #/g' $f
-    perl -pi -e 's/NpcBot.Enable.Arena(\s*)=/NpcBot.Enable.Arena = 1 #/g' $f
-    perl -pi -e 's/NpcBot.WanderingBots.BG.Enable(\s*)=/NpcBot.WanderingBots.BG.Enable = 1 #/g' $f
+    perl -pi -e 's/^NpcBot.MaxBots(\s*)=/NpcBot.MaxBots = 39 #/g' $f
+    perl -pi -e 's/^NpcBot.Enable.Raid(\s*)=/NpcBot.Enable.Raid = 1 #/g' $f
+    perl -pi -e 's/^NpcBot.Enable.BG(\s*)=/NpcBot.Enable.BG = 1 #/g' $f
+    perl -pi -e 's/^NpcBot.Enable.Arena(\s*)=/NpcBot.Enable.Arena = 1 #/g' $f
+    perl -pi -e 's/^NpcBot.WanderingBots.BG.Enable(\s*)=/NpcBot.WanderingBots.BG.Enable = 1 #/g' $f
 done
 
 mods='
@@ -46,7 +46,7 @@ https://github.com/trickerer/mod-autobalance
 cat <<EOF >>./etc/worldserver.conf
 Eluna.Enabled = true
 Eluna.TraceBack = false
-Eluna.ScriptPath = "./lua_scripts"
+# Eluna.ScriptPath = "./lua_scripts"
 Eluna.PlayerAnnounceReload = false
 
 LearnSpells.Enable = 1
@@ -65,6 +65,9 @@ AutoBalance.Enable.Global=1
 SoloLFG.Enable = 1
 SoloLFG.Announce = 1
 
+Arena1v1.Enable = 1
+Arena1v1.Announcer = 1
+
 Skip.Deathknight.Starter.Announce.enable = 1
 Skip.Deathknight.Starter.Enable = 1
 
@@ -74,11 +77,9 @@ AOELoot.MailEnable = 0
 GainHonorGuard.Enable = 1
 GainHonorGuard.Announce = 1
 
-AuctionHouseBot.DEBUG = 1
-AuctionHouseBot.DEBUG_FILTERS = 1
 AuctionHouseBot.EnableSeller = 1
-AuctionHouseBot.EnableBuyer = 0
-AuctionHouseBot.Account = 2
+AuctionHouseBot.EnableBuyer = 1
+AuctionHouseBot.Account = 3
 AuctionHouseBot.GUID = 1
 EOF
 
