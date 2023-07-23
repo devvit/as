5e00bb9bc2629bc16dfc282f1aa117c486cbd001
ymca <admin@example.com>
2023-07-23 14:40:30 +0800

    Sun Jul 23 14:40:29 CST 2023  M makeconf.sh

--- makeconf.sh
+++ makeconf.sh
@@ -1,32 +1,36 @@
 #!/usr/bin/env bash
 
+shopt -s globstar
+
 whiz=100
 
 for distfile in $(ls ./etc/**/*.dist); do
     f=$(dirname $distfile)/$(basename $distfile .dist)
     echo cp $distfile $f
-    # cp $distfile $f
-    # perl -pi -e 's/127.0.0.1;3306;acore;acore;/127.0.0.1;4306;acore;acore;/g' $f
-    # perl -pi -e 's/^DataDir(\s*)=/DataDir = ".\/data" #/g' $f
-    # perl -pi -e 's/^SOAP.Enabled(\s*)=/SOAP.Enabled = 1 #/g' $f
+    cp $distfile $f
+    perl -pi -e 's/127.0.0.1;3306;acore;acore;/127.0.0.1;4306;acore;acore;/g' $f
+    perl -pi -e 's/^DataDir(\s*)=/DataDir = ".\/data" #/g' $f
+    perl -pi -e 's/^SOAP.Enabled(\s*)=/SOAP.Enabled = 1 #/g' $f
 
-    # perl -pi -e "s/^Rate.XP.Kill(\s*)=/Rate.XP.Kill = $whiz #/g" $f
-    # perl -pi -e "s/^Rate.XP.Quest(\s*)=/Rate.XP.Quest = $whiz #/g" $f
-    # perl -pi -e "s/^Rate.XP.Quest.DF(\s*)=/Rate.XP.Quest.DF = $whiz #/g" $f
-    # perl -pi -e "s/^Rate.XP.Explore(\s*)=/Rate.XP.Explore = $whiz #/g" $f
-    # perl -pi -e "s/^Rate.XP.Pet(\s*)=/Rate.XP.Pet = $whiz #/g" $f
+    perl -pi -e "s/^Rate.XP.Kill(\s*)=/Rate.XP.Kill = $whiz #/g" $f
+    perl -pi -e "s/^Rate.XP.Quest(\s*)=/Rate.XP.Quest = $whiz #/g" $f
+    perl -pi -e "s/^Rate.XP.Quest.DF(\s*)=/Rate.XP.Quest.DF = $whiz #/g" $f
+    perl -pi -e "s/^Rate.XP.Explore(\s*)=/Rate.XP.Explore = $whiz #/g" $f
+    perl -pi -e "s/^Rate.XP.Pet(\s*)=/Rate.XP.Pet = $whiz #/g" $f
 
-    # perl -pi -e "s/^Rate.Honor(\s*)=/Rate.Honor = $whiz #/g" $f
-    # perl -pi -e "s/^Rate.ArenaPoints(\s*)=/Rate.ArenaPoints = $whiz #/g" $f
+    perl -pi -e "s/^Rate.Honor(\s*)=/Rate.Honor = $whiz #/g" $f
+    perl -pi -e "s/^Rate.ArenaPoints(\s*)=/Rate.ArenaPoints = $whiz #/g" $f
 
-    # perl -pi -e "s/^Rate.Drop.Money(\s*)=/Rate.Drop.Money = $whiz #/g" $f
+    perl -pi -e "s/^Rate.Drop.Money(\s*)=/Rate.Drop.Money = $whiz #/g" $f
 
-    # perl -pi -e 's/^NpcBot.MaxBots(\s*)=/NpcBot.MaxBots = 39 #/g' $f
-    # perl -pi -e 's/^NpcBot.Enable.Raid(\s*)=/NpcBot.Enable.Raid = 1 #/g' $f
-    # perl -pi -e 's/^NpcBot.Enable.BG(\s*)=/NpcBot.Enable.BG = 1 #/g' $f
-    # perl -pi -e 's/^NpcBot.Enable.Arena(\s*)=/NpcBot.Enable.Arena = 1 #/g' $f
-    # perl -pi -e 's/^NpcBot.WanderingBots.Continents.Count(\s*)=/NpcBot.WanderingBots.Continents.Count = 10 #/g' $f
-    # perl -pi -e 's/^NpcBot.WanderingBots.BG.Enable(\s*)=/NpcBot.WanderingBots.BG.Enable = 1 #/g' $f
+    perl -pi -e 's/^NpcBot.MaxBots(\s*)=/NpcBot.MaxBots = 39 #/g' $f
+    perl -pi -e 's/^NpcBot.Enable.Raid(\s*)=/NpcBot.Enable.Raid = 1 #/g' $f
+    perl -pi -e 's/^NpcBot.Enable.BG(\s*)=/NpcBot.Enable.BG = 1 #/g' $f
+    perl -pi -e 's/^NpcBot.Enable.Arena(\s*)=/NpcBot.Enable.Arena = 1 #/g' $f
+    perl -pi -e 's/^NpcBot.WanderingBots.Continents.Count(\s*)=/NpcBot.WanderingBots.Continents.Count = 10 #/g' $f
+    perl -pi -e 's/^NpcBot.WanderingBots.BG.Enable(\s*)=/NpcBot.WanderingBots.BG.Enable = 1 #/g' $f
+
+    perl -pi -e 's/^AOELoot.MailEnable(\s*)=/AOELoot.MailEnable = 0 #/g' $f
 done
 
 mods='
@@ -43,43 +47,6 @@ https://github.com/azerothcore/mod-skip-dk-starting-area
 https://github.com/azerothcore/mod-solo-lfg
 https://github.com/trickerer/mod-autobalance
 '
-
-# cat <<EOF >>./etc/worldserver.conf
-
-# Eluna.Enabled = true
-# Eluna.TraceBack = false
-# # Eluna.ScriptPath = "./lua_scripts"
-# Eluna.PlayerAnnounceReload = false
-
-# LearnSpells.Enable = 1
-# LearnSpells.Announce = 1
-# LearnSpells.LearnAllOnFirstLogin = 0
-# LearnSpells.OnFirstLogin = 0
-
-# RandomEnchants.AnnounceOnLogin=1
-# RandomEnchants.OnLoginMessage="This server is running a RandomEnchants Module."
-# RandomEnchants.OnLoot=1
-# RandomEnchants.OnCreate=1
-# RandomEnchants.OnQuestReward=1
-
-# AutoBalance.Enable.Global=1
-
-# SoloLFG.Enable = 1
-# SoloLFG.Announce = 1
-
-# Arena1v1.Enable = 1
-# Arena1v1.Announcer = 1
-
-# Skip.Deathknight.Starter.Announce.enable = 1
-# Skip.Deathknight.Starter.Enable = 1
-
-# AOELoot.Enable = 1
-# AOELoot.MailEnable = 0
-
-# GainHonorGuard.Enable = 1
-# GainHonorGuard.Announce = 1
-
-# EOF
 
 # node test.js 'account create hanmeimei helloworld'
 # node test.js 'account set gmlevel hanmeimei 3 -1'
