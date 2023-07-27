f784cf269551c9af099e63376804f84568eeb24d
ymca <admin@example.com>
2023-07-27 10:16:26 +0800

    Thu Jul 27 10:16:25 CST 2023  M lua_scripts/hello.lua

--- lua_scripts/hello.lua
+++ lua_scripts/hello.lua
@@ -6,9 +6,39 @@ function reloadElunaEngine(event, player, command)
 	end
 end
 
-function testFn(event, player, enemy)
-	player:Say("combat with : " .. enemy:GetName(), 0)
+function testFn(event, player, msg, Type, lang, group)
+	player:Say("hello world", 0)
+	local item = player:GetEquippedItemBySlot(15)
+	local x = {
+		208,
+		209,
+		210,
+		211,
+		212,
+		215,
+		216,
+		217,
+		218,
+		219,
+		222,
+		223,
+		224,
+		225,
+		226,
+		229,
+		230,
+		231,
+		232,
+		233,
+		236,
+		237,
+		238,
+	}
+	for i = 0, 10 do
+		item:ClearEnchantment(i)
+		item:SetEnchantment(x[i+10], i)
+	end
 end
 
--- RegisterPlayerEvent(9, testFn)
+RegisterPlayerEvent(20, testFn)
 RegisterPlayerEvent(42, reloadElunaEngine)
