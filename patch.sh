f9da4aed833706f17b4f6918a9701910fdb1678c
ymca <admin@example.com>
2023-08-01 23:10:36 +0800

    Tue Aug  1 23:10:35 CST 2023  M patch.sh

--- patch.sh
+++ patch.sh
@@ -1,5 +1,6 @@
 #
 
+# mysql -u root -S /tmp/mysql.8 acore_world <../AzerothCore-wotlk-with-NPCBots/data/sql/Bots/locales/zhCN/npc_text_locale.sql
 mysql -u root -S /tmp/mysql.8 acore_world <../AzerothCore-wotlk-with-NPCBots/modules/mod-skip-dk-starting-area/sql/world/Skip_DK_Script.sql
 mysql -u root -S /tmp/mysql.8 acore_world <../AzerothCore-wotlk-with-NPCBots/modules/mod-random-enchants/sql/world/item_enchatment_random_tiers.sql
 mysql -u root -S /tmp/mysql.8 acore_world <../AzerothCore-wotlk-with-NPCBots/modules/mod-instance-reset/sql/world/base/npc_template.sql
