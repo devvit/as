cd51861bd1b48070ab0c80c210090892a76f1ab4
ymca <admin@example.com>
2023-08-02 20:59:32 +0800

    Wed Aug  2 20:59:31 CST 2023  M 1.sql

--- 1.sql
+++ 1.sql
@@ -1,1 +1,5 @@
 DROP DATABASE IF EXISTS `acore_playerbots`;
+
+CREATE DATABASE `acore_playerbots` DEFAULT CHARACTER SET UTF8MB4 COLLATE utf8mb4_general_ci;
+
+GRANT ALL PRIVILEGES ON `acore_playerbots` . * TO 'acore'@'%' WITH GRANT OPTION;
