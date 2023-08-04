203072d20148362383d7bdb6d6c4fb37b0ecd878
ymca <admin@example.com>
2023-08-04 19:10:31 +0800

    Fri Aug  4 19:10:30 CST 2023  M custom.sql

--- custom.sql
+++ custom.sql
@@ -1,5 +1,3 @@
-DELETE FROM `mod_mko_map_lock` WHERE `mapId`=0 AND `zoneID` IN (267);
-INSERT INTO `mod_mko_map_lock` (`mapId`, `zoneID`, `comment`) VALUES (0, 267, "Gilneas");
 DROP TABLE IF EXISTS `mod_auctionhousebot_pro_items`;
 CREATE TABLE IF NOT EXISTS `mod_auctionhousebot_pro_items` (`Item` int unsigned NOT NULL DEFAULT 0);
 INSERT INTO `mod_auctionhousebot_pro_items` (`Item`) VALUES 
