d7616f678ed7a24b287caa312684b50963611bce
ymca <admin@example.com>
2023-08-04 15:55:54 +0800

    Fri Aug  4 15:55:53 CST 2023  M spawn.sh

--- spawn.sh
+++ spawn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for i in {1..5}; do
-  echo node cmd.js ".npcbot spawnto $i 0 -1209.2574462891 1388.7039794922 54.492282867432 0"
+for i in {70800..71999}; do
+  node cmd.js ".npcbot spawnto $i 0 -1209.2574462891 1388.7039794922 54.492282867432 0"
 done
