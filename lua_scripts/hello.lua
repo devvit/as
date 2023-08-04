71b7b0c225baa00d17326b92bda4143d34f58f9b
ymca <admin@example.com>
2023-08-04 10:30:31 +0800

    Fri Aug  4 10:30:30 CST 2023  M ac.patch  M lua_scripts/hello.lua

--- lua_scripts/hello.lua
+++ lua_scripts/hello.lua
@@ -7,12 +7,16 @@ function reloadElunaEngine(event, player, command)
 end
 
 function testFn(event, player, item, bag, slot)
-	player:Say("hello world", 0)
+	-- player:Say("hello world", 0)
 	local x = player:GetX()
 	local y = player:GetY()
 	local z = player:GetZ()
 	local o = player:GetO()
-	player:Say("At " .. x .. " , " .. y .. " , " .. z .. " , " .. o, 0)
+	local mapid = player:GetMapId()
+	player:Say("At " .. mapid .. " , " .. x .. " , " .. y .. " , " .. z .. " , " .. o, 0)
+	print("At " .. x .. " , " .. y .. " , " .. z .. " , " .. o)
+	-- local godness = GetPlayerByName("Playa")
+	-- player:Say(godness:GetX(), 0)
 end
 
 RegisterPlayerEvent(29, testFn)
